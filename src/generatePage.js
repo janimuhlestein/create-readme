bccc841a76f486ca81eae17b9d58d1e4921ff548
Jani Muhlestein <janimuhlestein@gmail.com>
2020-09-26 21:21:01 -0600

    removed extra space

--- src/generatePage.js
+++ src/generatePage.js
@@ -2,25 +2,25 @@ getBadge = (license) => {
     //depending on the license type, return the badge
     if(license === 'MIT') {
         return `
-        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 
         `
     } else if(license === 'Apache') {
         return `
-        [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
         `
     } else if (license === 'GNU') {
         return `
-        [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
         `
     } else if (license === 'Mozilla') {
         return `
-        [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
         `
     } else if(license === "Open Data Commons") {
         return `
-        [![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)
+    [![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)
         `
     }
 
